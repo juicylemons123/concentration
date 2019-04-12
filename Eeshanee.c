05f57aa2ecded6f76cf9744178ecdd4eda152212
faiyaz.rahman <faiyaz.rahman@invbots.com>
2019-04-12 21:01:10 +0800

    added a few more things

--- Eeshanee.c
+++ Eeshanee.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 typedef struct tcard{
 char rank;
@@ -6,7 +8,50 @@ char color;
 int  hidden=1;
 }card;
 
-card board[52];
+card *board;
+
+void CardSwap (card* x, card*y)
+{
+ card temp;
+ temp.rank=*x.rank; temp.color=*x.color;
+ *x.rank=*y.rank; *x.color=*y.color;
+ *y.rank=temp.rank; *y.color=temp.color;
+}
+
+void GameInit(card *tboard)
+{
+srand(time(NULL));
+char rank[13]=['2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'J', 'K', 'Q']
+char color[4]=['C', 'D', 'S', 'H']
+
+for (int i=0; i<4; i++)
+{
+ for (int j=0; j<13; j++)
+ {
+  *tboard[ (i*13)+j ].rank = rank[j];
+  *tboard[ (i*13)+j ].color= color[i];
+ }
+}
+
+for (int i=0, i
+
+
+}
+
+void GameStart(int *tmode)
+{
+board=(card *)(52*sizeof(card));
+printf("**************************\n");
+printf("*Welcome to Concentration*\n");
+printf("**************************\n");
+printf("Please choose a mode of the game:\n1. Human vs. Computer\n2. Computer vs. Computer\n0. Quit")
+ while (*tmode!=0 || *tmode!=1 || *tmode!=2)
+ {
+ scanf("Your choice: %d\n", tmode);
+ if (*tmode!=0 || *tmode!=1 || *tmode!=2) printf("Rejected, Enter a valid number!\n")
+ }
+GameInit(board);
+}
 
 void PrintBoard(card board[])
 {
